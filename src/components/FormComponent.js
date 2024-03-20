19bf1f234c44e8cb716b77d45735f310978e481a
Passon <passon871@gmail.com>
2024-03-20 17:07:14 +0700

    useEffect

--- src/components/FormComponent.js
+++ src/components/FormComponent.js
@@ -1,10 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./FormComponent.css";
 import { v4 as uuidv4 } from "uuid";
 
 const FormComponent = (props) => {
+  //console.log("Render Form Component");
   const [title, setTitle] = useState("");
   const [amount, setAmount] = useState(0);
+  const [formValid, setFormValid] = useState(false);
 
   const inputTitle = (event) => {
     setTitle(event.target.value);
@@ -21,11 +23,15 @@ const FormComponent = (props) => {
       title: title,
       amount: Number(amount),
     };
-    console.log(itemData);
     props.onAddItem(itemData);
     setTitle("");
     setAmount(0);
   };
+
+  useEffect(() => {
+    const checkData = title.trim().length > 0 && amount !== 0;
+    setFormValid(checkData);
+  }, [title, amount]);
 
   return (
     <div>
@@ -49,7 +55,7 @@ const FormComponent = (props) => {
           />
         </div>
         <div>
-          <button type="submit" className="btn">
+          <button type="submit" className="btn" disabled={!formValid}>
             เพิ่มข้อมูล
           </button>
         </div>
